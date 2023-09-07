fefe99bcb339b6bb0700eab91fde6bc097874f37
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2023-09-07 21:14:20 +0300

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - using puts to print text to stdout
+ * @void - brings void()
+ * Return: 0
+ */
+
 int main(void)
 {
-        char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
-        puts(Mystr);
-        return (0);
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return (0);
 }
