39ccf4d4d24d53a36f6fb8bf223110fb281d0d5d
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2023-09-08 22:37:38 +0300

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing, to stdout, alphabet
+ * in lowercase
+ * @void - blanks
+ * Return: Always 0
+ */
+int main(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	for (int i = 0; i <= 27; i++)
+	{
+		putchar(str[i];
+	}
+	putchar('\n');
+	return (0);
+}
