638e86d8b0ad57e1ce483432fc9cc25d970590ca
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2023-09-19 20:36:58 +0300

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,13 @@
-#include <unistd.c>
+#include <unistd.h>
+
 /**
- * putchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: the character to print
- * Return : on sucess 1
- * on error, -1 is referred and errno is set
+ * Return: on success 1
+ * on error, -1 is returned and errno is set approp.
  */
 
 int _putchar(char c)
 {
-	return (write(1, &write, 1);
+        return (write(1, &c, 1));
 }
