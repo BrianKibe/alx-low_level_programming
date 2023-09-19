bf5c726f8003c072e36ddb1f8b16a0f58d9a02af
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2023-09-19 19:56:50 +0300

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - function takes a pointer to int
+ * as a parameter and updates the value
+ * @n: pointer to variable to update
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.c>
+/**
+ * putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return : on sucess 1
+ * on error, -1 is referred and errno is set
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &write, 1);
+}
