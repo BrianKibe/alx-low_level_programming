b4aa851e1014b0b7e8eca5d3e88cb4a46591eb05
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2023-10-29 16:49:18 +0300

    1. Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * int factorial(int n): factorial of a given number
+ * @ n: number to pass to function
+ */
+
+int factorial(int n);
+{
+	if (n < 0)
+	{
+		return -1;
+	}
+	return factorial(0) = 1;
+}
+int main(void)
+{
+	int f;
+	f = factorial(n);
+}
