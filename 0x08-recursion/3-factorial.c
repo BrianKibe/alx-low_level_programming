35101c1ff99631fdddd2ae7434b63a4a3ac14285
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2023-10-29 17:04:20 +0300

    3. You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,16 +4,11 @@
  * @ n: number to pass to function
  */
 
-int factorial(int n);
+int factorial(int n)
 {
 	if (n < 0)
-	{
-		return -1;
-	}
-	return factorial(0) = 1;
-}
-int main(void)
-{
-	int f;
-	f = factorial(n);
+		return (-1);
+	else if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
 }
