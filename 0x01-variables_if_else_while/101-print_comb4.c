c0701ed13fb5c58597d06e3116a17f4ecd7ec523
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2024-01-24 15:28:36 +0300

    0. A library is not a luxury but one of the necessities of life

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,24 +9,41 @@ int main(void)
 	int n;
 	int n1;
 	int n2;
+	int n3;
 
-	for (n = 0; n <= 7; n++)
+	for (n = 0; n <= 6; n++)
 	{
-		for (n1 = n + 1; n1 <= 8; n1++)
+		for (n1 = n + 1; n1 <= 7; n1++)
 		{
-			for (n2 = n1 + 1; n2 <= 9; n2++)
+			for (n2 = n1 + 1; n2 <= 8; n2++)
 			{
-				putchar('0' + n);
-				putchar('0' + n1);
-				putchar('0' + n2);
-		if (!(n == 7 && n1 == 8 && n2 == 9))
-		{
-			putchar(44);
-			putchar(' ');
-		}
-			}
-		}
-	}
+				for (n3 = n2 + 1; n3 <= 9; n3++)
+				{
+					putchar('0' + n);
+					putchar('0' + n1);
+				if (!(n == 6 && n1 == 7))
+				{
+					putchar(' ');
+
+					if(!(n2 == 8 && n3 == 9))
+					{
+						putchar('0' + n2);
+						putchar('0' + n3);
+
+			if (!(n3 == 9 && n4 == 9))
+			{
+				putchar(44);
+				putchar(' ');
+
 	putchar('\n');
 	return (0);
+			
+					}
+				}
+			}
+
+		}
+	}
+}
+}
 }
