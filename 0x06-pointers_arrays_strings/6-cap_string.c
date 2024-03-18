520b0132395fb3f2ccc5d0be4a6851e65389ed29
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2024-03-18 14:15:22 +0300

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,53 +1,56 @@
 #include "main.h"
+
 /**
- * cap_string - capitalizes words in string
- * @s: string to capitalize
- *
- * Return: nothing
+ * lowercase -  Checks if a character is lowercase
+ * @c: character
+ * Return: 1 if lower, 0 otherwise
  */
-
-int isLowercase (char c)
+int lowercase(char c)
 {
-	return c >= 'a' && c <= 'z';
+	return (c >= 'a' && c <= 'z');
 
 }
-
-char capitalize (char c)
+/**
+ * capitalize - capitalizes a character
+ * @c: character to check
+ * Return: capitalized character
+ */
+char capitalize(char c)
 {
-	if (isLowercase(c))
-		return c - ('a' - 'A');
+	if (lowercase(c))
+		return (c - ('a' - 'A'));
 	else
-		return c;
+		return (c);
 }
+/**
+ * cap_string - Capitalizes words in a string
+ * @s: String to capitalize
+ *
+ * Return: Capitalized string
+ */
 
 char *cap_string(char *s)
 {
-
-	
 	int i = 0;
 	int capitalize_next = 1;
 
 	while (s[i] != '\0')
 	{
-		if (capitalize_next && isLowercase(s[i]))
-				{
-					s[i] = capitalize(s[i]);
-				}
+		if (capitalize_next && lowercase(s[i]))
+			s[i] = capitalize(s[i]);
 
-		if (s[i] == ' ' || s[i] == ',' || s[i] == ';' || s[i] == '?' || s[i] == '(' ||
-		   s[i] == ')' || s[i] == '{' || s[i] == '}' || s[i] == '!' || s[i] == '"' ||
+		if (s[i] == ' ' || s[i] == ',' ||
+s[i] == ';' || s[i] == '?' || s[i] == '(' ||
+s[i] == ')' || s[i] == '{' || s[i] == '}' ||
+		   s[i] == '!' || s[i] == '"' ||
 		  s[i] == '.' || s[i] == '\n' || s[i] == '\t')
 		{
 			capitalize_next = 1;
 			}
-
-	
 		else
 		{
-		
 			capitalize_next = 0;
 			}
-		
 		i++;
 	}	return s;
 }
