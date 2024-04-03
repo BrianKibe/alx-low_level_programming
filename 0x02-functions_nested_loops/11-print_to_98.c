585d47b8393615b1a2da990558ac1d3399d1b624
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2024-04-03 17:26:43 +0300

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,28 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
- * print_to_98- function that prints natural numbers
+ * print_to_98 - prints numbers
  * @n: starting point
- * return: null
  */
-
 void print_to_98(int n)
 {
-	if (n > = 98)
-	{
-		while (n > 98)
-		{
-			printf("%d, ", n--);
-			printf("%d\n", n);
-		}
-		else
-		{
-			while (n < 98)
-				printf("%d, ", n++);
-				printf("%d\n", n);
-	}
+	int i = n;
 
+	printf("%d\n", n);
+
+	if (i < 98)
+	{
+		for (i = n + 1; i <= 98; i++)
+		{
+			printf(", %d", i);
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n - 1; i >= 98; i--)
+		{
+			printf(", %d", i);
+		}
+	}
+	printf("\n");
 }
