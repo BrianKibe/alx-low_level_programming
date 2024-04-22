b0cffe834bb4a30c86bdad405302ee19711ec87a
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2024-04-22 15:40:24 +0300

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,37 +1,47 @@
 #include "main.h"
 /**
- * print_times_table - prints the times table
- * @n: number to use
- * Return: nothing, it's void
+ * print_times_table - prints times table
+ * @n: number to check
+ * Return: nothing, func is void
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
+	if (n > 15 || n < 0)
 	{
 		return;
 	}
 	else
 	{
-		int i, j, prod;
+		int i = 0;
 
-		for (i = 0; i <= n; i++)
+		while (i <= n)
 		{
-			for (j = 0; j <= n; j++)
+			int j = 0, prod;
+
+			while (j <= n)
 			{
 				prod = i * j;
-
 				if (j != 0)
+				{
 					_putchar(',');
-					if (prod < 10)
-						_putchar(' ');
-					if (prod < 100)
-						_putchar(' ');
 					_putchar(' ');
+					if (prod >= 10 && prod < 100)
+					{
+						_putchar(' ');
+					}
+					if (prod < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+
+				}
 				if (prod >= 100)
+				{
 					_putchar(prod / 100 + '0');
 					_putchar((prod % 100) / 10 + '0');
 					_putchar((prod % 100) % 10 + '0');
-
+				}
 				else if (prod >= 10 && prod <= 99)
 				{
 					_putchar(prod / 10 + '0');
@@ -41,7 +51,10 @@ void print_times_table(int n)
 				{
 					_putchar(prod + '0');
 				}
-			} _putchar(10);
-		} _putchar(10);
+				j++;
+			}
+		_putchar(10);
+		i++;
+		}
 	}
 }
