de09ff443bde8b045c982511103731c89f5aebeb
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2024-04-30 13:58:24 +0300

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - 4 million
+ * Return: Always 0
+ */
+int main(void)
+{
+
+	int i;
+
+	long int t1 = 1;
+	long int t2 = 1;
+	long int sum = 0;
+
+	long int t3 = t1 + t2;
+
+	t1 = t2;
+	t2 = t3;
+
+	while (t3 < 4000000 || t3 % 2 == 0)
+	{
+		sum += t3;
+		t3++;
+
+	}
+	printf("%ld", sum);
+	printf("\n");
+	return (0);
+}
