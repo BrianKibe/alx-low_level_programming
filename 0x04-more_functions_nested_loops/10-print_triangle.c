50eea5e3704ffef1d7771fed0e1e1c8d1a7afabb
BrianKibe <79489099+BrianKibe@users.noreply.github.com>
2024-05-15 18:37:53 +0300

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle(int size)
+ * @size: number to use
+ * Return: Always 0
+ */
+void print_triangle(int size)
+{	
+	int row, column;
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	
+
+	for (row = 0; row < size; row++)
+	{
+		for (column = 0; column < size - row - 1; column++)
+		{
+			_putchar(' ');
+		}
+		for (column = 0; column <= row; column++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
